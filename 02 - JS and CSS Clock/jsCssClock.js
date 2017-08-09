dbfba30060c348a3db9bccd610cd1a0f827005a3
Dylan Sheffer <dsheffer16@Dylans-MacBook-Pro.local>
2017-08-09 19:07:19 -0400

    Used Transformations, animations, Date, and rotate

---
+++ 02 - JS and CSS Clock/jsCssClock.js
@@ -0,0 +1,24 @@
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    // Gives us a unit that can be plotted on a circle + 90 degrees because we offset it by 90 initially
+    // (The percent of the minute * 360 degrees)
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+// Triggers setDate every second
+setInterval(setDate, 1000);
